c6b89ee83282bb9f4c17d0f1282aca5d4c0f4f5f
Saim Amir <saimxamir@gmail.com>
2023-12-11 11:04:32 -0500

    add win and draw check methods, update game loop logic

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -15,17 +15,41 @@ namespace TicTacToe
 
             //Tracking current player (X or 0)
             char currentPlayer = 'X';
+            bool gameEnd = false;
 
-
-            while (true)
+            while (!gameEnd)
             {
                 // Print the game board to the console
 
                 PrintGameBoard(board);
+
+                
                 PlayerMoveInput(currentPlayer, board);
 
-                //Switch Player
-                currentPlayer = (currentPlayer == 'X' ? '0' : 'X');
+                //Check for Win
+                if (WinCheck(board))
+                {
+                    PrintGameBoard(board);
+
+                    Console.WriteLine($"Player {currentPlayer} wins!");
+                    gameEnd = true;
+                }
+                
+                else
+                {
+                    // Check for a draw
+                    if (DrawCheck(board))
+                    {
+                        PrintGameBoard(board);
+                        Console.WriteLine("It's a draw!");
+                        gameEnd = true;
+                    }
+                    else
+                    {
+                        // Switch Player
+                        currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
+                    }
+                }
 
 
             }
@@ -74,20 +98,61 @@ namespace TicTacToe
             int col = int.Parse(Console.ReadLine());
             Console.WriteLine(row);
 
-            if (board[row,col] == ' ')
+            if (board[row,col] == ' ' && row >= 0 && row < 3 && col >= 0 && col < 3)
             {
-                //If cell is empty, update it
+                //If cell is empty,or value is in the bounds then update it
                 board[row, col] = player;
             }
             else
             {
-                //Cell is occupied. Inform player.
+                //Cell is occupied or value out of bounds. Inform player.
                 Console.WriteLine("Your move was invalid as the selected cell is already occupied. Try Again.");
                 PlayerMoveInput(player, board);
             }
 
         }
-        
+
+        static bool WinCheck(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                // Perform Hz. Check
+                if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                    return true;
+
+                // Perform Vert. Check
+                if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                    return true;
+            }
+
+            // Perform Diagonal Check
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                return true;
+
+            // Reverse Diag. Check
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                return true;
+
+            return false;
+        }
+
+
+        // Method to check for a draw
+        static bool DrawCheck(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false; // If there's an empty cell, the game is not a draw yet
+                    }
+                }
+            }
+            return true; // If all cells are filled, it's a draw
+        }
+
 
 
     }
