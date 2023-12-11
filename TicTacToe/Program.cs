809bca3bf73b4dfa5c26890ed7afe6af4aa2ebb4
Saim Amir <saimxamir@gmail.com>
2023-12-11 09:56:33 -0500

    add method to display board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace TicTacToe
+﻿using System;
+
+namespace TicTacToe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Define the game board
+            int[,] board = new int[3, 3];
+
+            // Print the game board to the console
+            PrintGameBoard(board);
+        }
+
+        // Method to print the game board
+        static void PrintGameBoard(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
